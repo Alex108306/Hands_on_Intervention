d665bf65af9bce11d2eb1d7927b875e87eba85d7
Alex108306 <giang.nht108201@gmail.com>
2026-03-31 17:28:32 +0200

    In progress implementing Lab 6

---
+++ Lab_6/lab6_mobile_manipulator.py
@@ -0,0 +1,96 @@
+from lab6_robotics import * # Includes numpy import
+import matplotlib.pyplot as plt
+import matplotlib.patches as patch
+import matplotlib.animation as anim
+import matplotlib.transforms as trans
+
+# Robot model
+d = np.zeros(3)                            # displacement along Z-axis
+theta = np.array([0.2, 0.5, 0.2])          # rotation around Z-axis
+alpha = np.zeros(3)                        # rotation around X-axis
+a = np.array([0.5, 0.5, 0.5])              # displacement along X-axis
+revolute = np.array([True, True, True])    # flags specifying the type of joints
+robot = MobileManipulator(d, theta, a, alpha, revolute)
+damping_factor = 0.1
+
+# Task definition
+
+tasks = [ 
+          Position2D("End-effector position", np.array([1.0, 0.5]).reshape(2,1), 5)
+        ] 
+
+# Simulation params
+dt = 1.0/60.0
+
+# Drawing preparation
+fig = plt.figure()
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2,2))
+ax.set_title('Simulation')
+ax.set_aspect('equal')
+ax.grid()
+ax.set_xlabel('x[m]')
+ax.set_ylabel('y[m]')
+rectangle = patch.Rectangle((-0.25, -0.15), 0.5, 0.3, color='blue', alpha=0.3)
+veh = ax.add_patch(rectangle)
+line, = ax.plot([], [], 'o-', lw=2) # Robot structure
+path, = ax.plot([], [], 'c-', lw=1) # End-effector path
+point, = ax.plot([], [], 'rx') # Target
+PPx = []
+PPy = []
+
+# Simulation initialization
+def init():
+    global tasks
+    line.set_data([], [])
+    path.set_data([], [])
+    point.set_data([], [])
+    return line, path, point
+
+# Simulation loop
+def simulate(t):
+    global tasks
+    global robot
+    global PPx, PPy
+    
+    ### Recursive Task-Priority algorithm
+    # Initialize null-space 
+    P = np.identity(robot.dof)
+    # Initialize output vector (joint velocity)
+    dq = np.zeros((robot.dof, 1))
+    # Loop over tasks
+    for i in range(len(tasks)):
+        # Update task state
+        tasks[i].update(robot)
+        # Padding Jacobian to match the dimension of the robot dof and compute the augmented Jacobian, then compute the DLS solution and accumulate velocity, and finally update the null-space projector
+        Jacobian_padded = np.pad(tasks[i].getJacobian(), ((0, 0), (0, robot.dof - tasks[i].getJacobian().shape[1])))
+        # Compute augmented Jacobian
+        J_bar = Jacobian_padded @ P
+        # Task definition
+        x_dot = tasks[i].getFeedforwardVelocity() + tasks[i].getGainMatrixK() @ tasks[i].getError()
+        # Compute task velocity & Accumulate velocity
+        dq = dq + DLS(J_bar, damping_factor) @ (x_dot - Jacobian_padded @ dq)
+        # Update null-space projector
+        P = P - np.linalg.pinv(J_bar) @ J_bar
+    ###
+
+    # Update robot
+    robot.update(dq, dt)
+    
+    # Update drawing
+    # -- Manipulator links
+    PP = robot.drawing()
+    line.set_data(PP[0,:], PP[1,:])
+    PPx.append(PP[0,-1])
+    PPy.append(PP[1,-1])
+    path.set_data(PPx, PPy)
+    point.set_data(tasks[0].getDesired()[0], tasks[0].getDesired()[1])
+    # -- Mobile base
+    eta = robot.getBasePose()
+    veh.set_transform(trans.Affine2D().rotate(eta[2,0]) + trans.Affine2D().translate(eta[0,0], eta[1,0]) + ax.transData)
+
+    return line, veh, path, point
+
+# Run simulation
+animation = anim.FuncAnimation(fig, simulate, np.arange(0, 10, dt), 
+                                interval=10, blit=True, init_func=init, repeat=True)
+plt.show()
